65ad86fff0d0cb6911aefd5de9eae4811d9badad
Pranav-Sthapit <164738041+Pranav-Sthapit@users.noreply.github.com>
2024-06-04 20:55:03 +0545

    this is a file newest one

--- HospitalQueueManagementSystem.java
+++ HospitalQueueManagementSystem.java
@@ -1,23 +1,190 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+//import java.util.Scanner;
+import java.io.*;
 
+class Patient implements Serializable{
+    int patientId;
+    String patientName;
+    int age;
+    int priority;
+    Patient next;
+    Patient(int a,String b,int c,boolean d){
+        patientId=a;
+        patientName=b;
+        age=c;
+        priority=(age<60)?1:2;
+        if(d)
+        {priority=3;}
+    }
+
+}
+class QueueLogic{
+    Patient start=null,end=null,temp,visit,i,j;
+    String calledPatient;
+    public void swap(Patient i,Patient j)
+    {   int t1;String t2;
+        t1=i.patientId;
+        i.patientId=j.patientId;
+        j.patientId=t1;
+        t2=i.patientName;
+        i.patientName=j.patientName;
+        j.patientName=t2;
+        t1=i.age;
+        i.age=j.age;
+        j.age=t1;
+        t1=i.priority;
+        i.priority=j.priority;
+        j.priority=t1;
+        
+    }
+    public void enqueue(Patient visit){
+        visit.next=null;
+        if(start==null)
+        {
+            start=visit;
+            end=visit;
+        }
+        else{
+            end.next=visit;
+            end=visit;
+        }
+    }
+    public void dequeue(){
+        if(start==null)
+        {
+            calledPatient="empty queue";
+        }else
+        {
+            System.out.println("the queue has been updated");
+            calledPatient="patient no "+ start.patientId+" please come forward";
+            if(start.next==null)
+            {
+                start=null;end=null;
+            }else{
+                start=start.next;
+            }
+        }
+    }
+    public void sort()
+    {   
+        for(i=start;i.next!=null;i=i.next)
+        {
+            for(j=start;j.next!=null;j=j.next){
+                if(j.priority<j.next.priority)
+                {
+                    swap(j,j.next);
+                }
+            }
+        }
+    }
+    public void display()
+    {
+        temp=start;
+        if(start==null)
+        {
+            System.out.println("enpty queue");
+        }else{
+        System.out.println("the orders of patient are");
+        while (temp!=null) {
+            System.out.println(temp.patientId);
+            System.out.println(temp.patientName);
+            System.out.println(temp.age+"\n\n");
+            temp=temp.next;
+        }
+    }
+    }
+    public void search(int p)
+    {int pos=1;
+        temp=start;
+        if(start==null)
+        {
+            System.out.println("enpty queue");
+        }
+        System.out.println("your info is");
+        while (temp!=null) {
+            if(p==temp.patientId){
+            System.out.println(temp.patientId);
+            System.out.println(temp.patientName);
+            System.out.println(temp.age);
+            System.out.println("position is "+pos+"\n");
+            }
+            pos++;
+            temp=temp.next;
+        }
+    }
+}
+class FileHandler{
+    public void writer(Patient s){
+        try{
+            FileOutputStream fos=new FileOutputStream("patients.txt");
+            ObjectOutputStream oos=new ObjectOutputStream(fos);
+            while(s!=null){
+            Patient temp=s;
+            oos.writeObject(temp);
+            s=s.next;
+            }
+            oos.close();
+            fos.close();
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public void reader(QueueLogic q){
+        try{   FileInputStream fis=new FileInputStream("patients.txt");
+            ObjectInputStream ois=new ObjectInputStream(fis);
+            while(fis.available()>0){
+            Patient temp=(Patient)ois.readObject();
+            temp.next=null;
+            q.enqueue(temp);
+            }
+            ois.close();
+            fis.close();
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
 public class HospitalQueueManagementSystem {
+    static int count;
+    QueueLogic q;
+    Patient p;
+    FileHandler f;
     private JFrame frame;
     private JPanel panel;
     private JButton staffButton,callButton,viewListButton;
-    private JLabel callLabel,demolbl;
+    private JLabel callLabel;
     private JButton patientButton;
     private JButton admissionButton;
     private JButton searchButton;
     private JButton backButton;
     private JTextField id;
     private JTextField name,age;
-    
+    private JRadioButton r1,r2;
+    private ButtonGroup grp;
 
      public HospitalQueueManagementSystem() {
+        q=new QueueLogic();
+        f=new FileHandler();
+        //idTracker();
+        //f.reader(q);
         frame = new JFrame("Hospital Queue Management System");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+        frame.addWindowListener( new WindowAdapter()
+        {
+        public void windowClosing(WindowEvent e)
+        {
+            //f.writer(q.start);
+            //idSetter();
+            System.exit(0);
+        }
+        });
+        
 
         // Create the main panel
         panel = new JPanel();
@@ -28,7 +195,6 @@ public class HospitalQueueManagementSystem {
         backButton=new JButton("back");
         panel.add(staffButton);
         panel.add(patientButton);
-        demolbl=new JLabel("res");
         staffButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e)
@@ -49,6 +215,24 @@ public class HospitalQueueManagementSystem {
         frame.setVisible(true);
 
      }
+    public static void idTracker()
+    {
+        try(DataInputStream d=new DataInputStream(new FileInputStream("tracker.txt"));){
+                count=d.readInt();
+                d.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void idSetter()
+    {
+        try(DataOutputStream d=new DataOutputStream(new FileOutputStream("tracker.txt"));){
+            d.writeInt(count);
+            d.close();
+    }catch(Exception e){
+        e.printStackTrace();
+    }
+    }
      public void setPatientInterface(){
             panel.removeAll();
             admissionButton=new JButton("admission");
@@ -79,6 +263,7 @@ public class HospitalQueueManagementSystem {
                 public void actionPerformed(ActionEvent e)
                 {
                     callLabel.setText("refer command panel");
+                    q.display();
                 }
             });
             backButton.addActionListener(new ActionListener() {
@@ -87,10 +272,10 @@ public class HospitalQueueManagementSystem {
                 {   panel.removeAll();
                     panel.add(staffButton);
                     panel.add(patientButton);
-                    panel.add(demolbl);
                 }
             });
-
+            frame.revalidate();
+        frame.repaint();
 
      }
      public void setStaffInterface(){
@@ -106,7 +291,8 @@ public class HospitalQueueManagementSystem {
                 @Override
                 public void actionPerformed(ActionEvent e)
                 {  //all details pending
-                    callLabel.setText("patient is called");
+                    q.dequeue();
+                    callLabel.setText(q.calledPatient);
                 }
             });
            viewListButton.addActionListener(new ActionListener() {
@@ -114,6 +300,7 @@ public class HospitalQueueManagementSystem {
                 public void actionPerformed(ActionEvent e)
                 {
                     callLabel.setText("refer command panel");
+                    q.display();
                 }
             });
             backButton.addActionListener(new ActionListener() {
@@ -123,11 +310,11 @@ public class HospitalQueueManagementSystem {
                     panel.removeAll();
                     panel.add(staffButton);
                     panel.add(patientButton);
-                    panel.add(demolbl);
-                    
                     
                 }
             });
+            frame.revalidate();
+        frame.repaint();
      }
      public void setSearchInterface(){
         panel.removeAll();
@@ -141,7 +328,9 @@ public class HospitalQueueManagementSystem {
             @Override
             public void actionPerformed(ActionEvent e)
             {  
-                callLabel.setText(id.getText());
+                int id2=Integer.parseInt(id.getText());
+                q.search(id2);
+                callLabel.setText("refer command panel for details");
             }
         });
         backButton.addActionListener(new ActionListener() {
@@ -152,21 +341,37 @@ public class HospitalQueueManagementSystem {
                 setPatientInterface();
             }
         });
+        frame.revalidate();
+        frame.repaint();
      }
      public void setAdmissionInterface(){
         panel.removeAll();
         name=new JTextField("enter name",20);
         age=new JTextField("enter age",10);
+        r1=new JRadioButton("critical");
+        r2=new JRadioButton("not critical");
+        grp=new ButtonGroup();
+        grp.add(r1);
+        grp.add(r2);
         panel.add(name);
         panel.add(age);
+        panel.add(r1);
+        panel.add(r2);
         panel.add(admissionButton);
         panel.add(callLabel);
         panel.add(backButton);
-        admissionButton.addActionListener(new ActionListener() {
+        admissionButton.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e)
             {  
-                callLabel.setText("your id is this");
+                String s1=name.getText();
+                int a1=Integer.parseInt(age.getText());
+                count++;
+                boolean result=r1.isSelected();
+                Patient p=new Patient(count,s1,a1,result);
+                q.enqueue(p);
+                q.sort();
+                callLabel.setText("your id is :"+count);
             }
         });
         backButton.addActionListener(new ActionListener() {
@@ -177,10 +382,11 @@ public class HospitalQueueManagementSystem {
                 setPatientInterface();
             }
         });
-
+        frame.revalidate();
+        frame.repaint();
      }
      public static void main(String[] args)
      {
-        HospitalQueueManagementSystem h=new HospitalQueueManagementSystem();
+        new HospitalQueueManagementSystem();
      }
 }
